e68bed0ad179296969321db6c1113bfedc293021
Mandy Arens <mandyfung93@gmail.com>
2020-04-15 21:40:02 -0600

    added username input field

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 const PORT = 8080; // default port 8080
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
 
 function generateRandomString() {  
   var string = "";
